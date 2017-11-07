c0fd88bfdd4ae03b4fa3b5df17c3e1a7a7c4da21
WolfgangBusch <32877802+WolfgangBusch@users.noreply.github.com>
2017-11-07 12:48:45 +0100

    Add files via upload

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
  * URL-Rewrite Addon
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Oktober 2017
+ * @version November 2017
  */
 echo rex_view::title(rex_i18n::msg('url_simple_rewrite'));
 rex_be_controller::includeCurrentPageSubPath();
--- pages/introduction.php
+++ pages/introduction.php
@@ -3,7 +3,7 @@
  * URL-Rewrite Addon
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Oktober 2017
+ * @version November 2017
  */
 require_once __DIR__.'/../help.php';
 ?>
