455d02e786e1a23ccd86a570cd45aa0cfffba4e9
WolfgangBusch <32877802+WolfgangBusch@users.noreply.github.com>
2017-11-07 12:45:46 +0100

    Add files via upload

--- boot.php
+++ boot.php
@@ -3,8 +3,9 @@
  * URL-Rewrite Addon
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Oktober 2017
+ * @version November 2017
  */
+require_once __DIR__.'/functions/function.general.php';
 require_once __DIR__.'/lib/class.url_simple_rewrite.php';
 require_once __DIR__.'/lib/class.fe_simple_output.php';
 rex_extension::register('URL_REWRITE',array('url_rewrite','rewrite'));
--- install.php
+++ install.php
@@ -3,6 +3,6 @@
  * URL-Rewrite Addon
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Oktober 2017
+ * @version November 2017
  */
 ?>
