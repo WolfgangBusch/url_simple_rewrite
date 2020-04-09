a18531239a1206a258bf308cce57c2d888181a32
Wolfgang Busch <wolfgang@busch-dettum.de>
2020-04-09 17:14:57 +0200

    Add files via upload

--- pages/introduction.php
+++ pages/introduction.php
@@ -3,8 +3,8 @@
  * URL-Rewrite Addon
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Juni 2019
+ * @version April 2020
  */
-$intro=file_get_contents(__DIR__.'/../README.md');
-echo substr($intro,strlen($this->getPackageId())+2);
+$my_package=$this->getPackageId();
+require_once rex_path::addon($my_package).'/help.php';
 ?>
